b7494c3071533192cac8e41748fb334e71cb20e3
DaNub <75505891+DaNubCoding@users.noreply.github.com>
2022-10-18 21:07:30 -0400

    exe

--- images.py
+++ images.py
@@ -8,10 +8,10 @@ import pygame
 
 pygame.display.set_mode((1, 1))
 
-TILE_DIR = "assets/textures/tiles"
+TILE_DIR = pathof("assets/textures/tiles")
 TILE_IMGS = {image[:-4]: scale(load(pathof(join(TILE_DIR, image))), (TILE_SIZE, TILE_SIZE)).convert() for image in listdir(TILE_DIR)}
 
-ITEM_DIR = "assets/textures/items"
+ITEM_DIR = pathof("assets/textures/items")
 ITEM_IMGS = {image[:-4]: load(pathof(join(ITEM_DIR, image))).convert_alpha() for image in listdir(ITEM_DIR)}
 
 SOLDIER1_IMG1 = pygame.image.load(pathof("assets/textures/soldier1_ross.png")).convert_alpha()
@@ -21,6 +21,5 @@ BULLET_IMG = pygame.image.load(pathof("assets/textures/bullet.png")).convert_alp
 SKULL_IMG = pygame.image.load(pathof("assets/textures/skull.png")).convert_alpha()
 PARCHMENT_IMG = pygame.image.load(pathof("assets/textures/parchment.png")).convert_alpha()
 FLAG_IMG = pygame.image.load(pathof("assets/textures/flag.png")).convert_alpha()
-
 
 pygame.display.quit()
